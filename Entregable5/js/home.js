5a024af704b2408ef75e9c040b7809726172c40c
Magali Medico <magamedico@gmail.com>
2021-11-29 00:32:13 -0300

    Agregado flujo de comentarios y escribir una publicación

--- Entregable5/js/home.js
+++ Entregable5/js/home.js
@@ -4,6 +4,14 @@ function inicio(){
     document.getElementById("inputPublicar").addEventListener("click", ()=>{
         location.href = "publicar.html";
     });
+
+    document.getElementById("crearComent").addEventListener("click", ()=>{
+        let div = document.getElementById("coment");
+        div.style.visibility = "visible";
+        div.style.height = "44px";
+        let publi = document.getElementById("publiUno");
+        publi.style.height = "344px";
+    });
 }
 
 document.addEventListener("DOMContentLoaded", inicio());
--- Entregable5/js/publicacion.js
+++ Entregable5/js/publicacion.js
@@ -8,6 +8,13 @@ function inicio(){
     document.getElementById("publicar").addEventListener("click", ()=>{
         location.href = "home.html";
     });
+
+    document.getElementById("insertarFoto").addEventListener("click", ()=>{
+        let img = document.getElementById("img");
+        img.style.visibility = "visible";
+        let text = document.getElementById("txtPubli");
+        text.style.height = "80px";
+    });
 }
 
 document.addEventListener("DOMContentLoaded", inicio());
